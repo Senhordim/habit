4a36e4d9bd3c187a6f0e3a6d3d647daab97aa394
Diego Collares <diego.collares@gmail.com>
2021-09-25 20:29:24 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Diego Figueira Collares on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
